22fba160284ad90351a4787905277f187f2658ba
William Brammer <williamedwardbrammer@gmail.com>
2024-01-31 05:52:41 +0900

    feat: implement states and functions to update the min max based on isgrouporder in a menu item. The tests do not run currently due to a problem with immer. But based on what I read of the test and the functionality it should pass the tests if they had the correct data being sent and pushed to the menu array

--- src/client/Components/PartySizeList.tsx
+++ src/client/Components/PartySizeList.tsx
@@ -1,25 +1,91 @@
 import { PartySize } from "../Pages/ShopBookingPage/PartySize";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Counter } from "./Counter";
 type Props = {
   partySize: PartySize;
 };
 
+interface MinMaxObject {
+  min: number,
+  max: number,
+}
+
 
 export const PartySizeList = ({ partySize }: Props): JSX.Element => {
   const party = partySize
-  console.log(party);
 
   const [adults, setAdults] = useState<number>(party.shop.minNumPeople || 1);
   const [children, setChildren] = useState<number>(0);
   const [babies, setBabies] = useState<number>(0);
   const [seniors, setSeniors] = useState<number>(0);
-  const [totalPartySize, setTotalPartySize] = useState<number>(party.shop.minNumPeople || 1)
-  const [maxNumberOfPeople, setMaxNumberOfPeople] = useState<number>(party.shop.maxNumPeople || 10)
-  const [minNumberOfPeople, setMinNumberOfPeople] = useState<number>(party.shop.minNumPeople || 1)
-  const [adultCount, setAdultCount] = useState<number>(party.shop.minNumPeople || 1)
+  const [totalPartySize, setTotalPartySize] = useState<number>(party.shop.minNumPeople || 1);
+  const [maxNumberOfPeople, setMaxNumberOfPeople] = useState<number>(party.shop.maxNumPeople || 10);
+  const [minNumberOfPeople, setMinNumberOfPeople] = useState<number>(party.shop.minNumPeople || 1);
+  const [adultCount, setAdultCount] = useState<number>(party.shop.minNumPeople || 1);
+  const [hasGroupOrder, setHasGroupOrder] = useState<boolean>(false);
+  const [hasMenu, setHasMenu] = useState<boolean>(false);
+ 
   
+  
+  useEffect(()=>{
+    if(party.shop.minNumPeople > party.shop.maxNumPeople){
+      throw new Error("min can never be greater than max")
+    }
+    if(party.shop.minNumPeople === 0 || party.shop.minNumPeople === -Infinity){
+      setMinNumberOfPeople(1);
+      setAdults(1);
+      setAdultCount(1);
+      setTotalPartySize(1);
+    }
+    if(party.shop.maxNumPeople === Infinity){
+      setMaxNumberOfPeople(10);
+    }
+    if(party.menu.length > 0){
+      setHasMenu(true);
+    }
+  },[]);
 
+  useEffect(()=>{
+    if(hasMenu){
+      hasMinMaxForMenu();
+    }
+  },[hasMenu]);
+
+  useEffect(()=>{
+    if(hasGroupOrder){
+      const newMinMax: MinMaxObject = findMinMaxForGroupOrder();
+      setMinNumberOfPeople(newMinMax.min);
+      setMaxNumberOfPeople(newMinMax.max);
+    }
+  },[hasGroupOrder]);
+
+  //helper
+  function hasMinMaxForMenu(){
+    for(let i = 0; i < party.menu.length; i ++){
+      if(party.menu[i].isGroupOrder){
+        setHasGroupOrder(true);
+        return;
+    }
+  }
+}
+
+  function findMinMaxForGroupOrder(){
+    let minMaxForMenu: MinMaxObject={
+      min: 0,
+      max: 0
+    };
+    
+    for(let i = 0; i < party.menu.length; i ++){
+      if(party.menu[i].minOrderQty > minMaxForMenu.min){
+        minMaxForMenu.min = party.menu[i].minOrderQty
+      }
+      if(party.menu[i].maxOrderQty > minMaxForMenu.max){
+        minMaxForMenu.max = party.menu[i].maxOrderQty
+      }
+    }
+
+    return minMaxForMenu;
+  }
 
   return <div data-testid="Party Size List">
     <label>
