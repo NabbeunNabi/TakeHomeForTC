ddef3d55bbcde49761c0991c1058da4c84e9e8e8
William Brammer <williamedwardbrammer@gmail.com>
2024-01-31 01:58:45 +0900

    fix: changed the destructuring of the imported object to a constant so that it would keep the this context in binding

--- src/client/Pages/ShopBookingPage/ShopBookingPage.tsx
+++ src/client/Pages/ShopBookingPage/ShopBookingPage.tsx
@@ -1,15 +1,15 @@
 import { useController } from "./useController";
 
 export function ShopBookingPage(): JSX.Element {
-  const { title, openCTA, renderModal } = useController();
+  const apiFromController = useController();
 
   return (
     <div>
-      <h1 data-testid="Shop Title">{title}</h1>
-      <button data-testid="Party Size CTA" onClick={openCTA}>
+      <h1 data-testid="Shop Title">{apiFromController.title}</h1>
+      <button data-testid="Party Size CTA" onClick={apiFromController.openCTA}>
         click here to set party size
       </button>
-      {renderModal()}
+      {apiFromController.renderModal()}
     </div>
   );
 }
